fb1fe405eca292af6e635d2567d516ecf7d4bf47
Kim Dao <kimdao@Kims-MacBook-Pro-2.local>
2022-02-26 15:45:37 -0800

    mocked data for exercises and saved exercises

---
+++ src/main/migrations/20220226232724-create-saved-exercise.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const savedExercisesTableName = 'SavedExercises';
+const savedExercises = [
+  {
+    userId: 'b70820ae-d0a3-411b-9217-0bf2370e7139',
+    exerciseId: 1,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    userId: 'b70820ae-d0a3-411b-9217-0bf2370e7139',
+    exerciseId: 2,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+];
+
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface
+      .createTable(savedExercisesTableName, {
+        userId: {
+          type: Sequelize.UUID,
+          primaryKey: true
+        },
+        exerciseId: {
+          type: Sequelize.UUID,
+          primaryKey: true
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      })
+      .then(() => {
+        return queryInterface.bulkInsert(savedExercisesTableName, savedExercises);
+      });
+  },
+
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable(savedExercisesTableName);
+  }
+};
