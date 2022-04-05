d0fde5cffc963d5daba1b02a615ae07008340318
Kim Dao <kimdao@dhcp-206-87-119-30.ubcsecure.wireless.ubc.ca>
2022-04-04 19:52:19 -0700

    added BE endpoint for adding exercises to workout

--- src/main/migration/20220406061730-create-workout-exercise.js
+++ src/main/migration/20220406061730-create-workout-exercise.js
@@ -2,43 +2,42 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface
-      .createTable('WorkoutExercise', {
-        workoutId: {
-          type: Sequelize.UUID,
-          primaryKey: true,
-          references: {
-            model: 'Workouts',
-            key: 'id',
-            as: 'workoutId',
-          },
+    return queryInterface.createTable('WorkoutExercise', {
+      workoutId: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        references: {
+          model: 'Workouts',
+          key: 'id',
+          as: 'workoutId',
         },
-        exerciseId: {
-          type: Sequelize.INTEGER.UNSIGNED,
-          primaryKey: true,
-          references: {
-            model: 'Exercises',
-            key: 'id',
-            as: 'exerciseId',
-          },
+      },
+      exerciseId: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        references: {
+          model: 'Exercises',
+          key: 'id',
+          as: 'exerciseId',
         },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        orderNum: {
-          allowNull: false,
-          type: Sequelize.INTEGER.UNSIGNED,
-          default: 1,
-        },
-      });
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      orderNum: {
+        allowNull: false,
+        type: Sequelize.INTEGER.UNSIGNED,
+        default: 1,
+      },
+    });
   },
 
   async down(queryInterface, Sequelize) {
     return queryInterface.dropTable('WorkoutExercise');
-  }
+  },
 };
