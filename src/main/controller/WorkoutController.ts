2fdb626f5c0c3da77f791d8d85974ff0639f907a
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-04-04 23:42:37 -0700

    removed unnecessary comments in add-orderNum-column migration and removed unnecessary imports in WorkoutController

--- src/main/controller/WorkoutController.ts
+++ src/main/controller/WorkoutController.ts
@@ -1,5 +1,1 @@
-import { Request, Response } from 'express';
-import db from '../model';
-import { Route } from '../constant/Route';
-
 export class WorkoutController {}
--- src/main/migration/20220404222042-add-orderNum-column.js
+++ src/main/migration/20220404222042-add-orderNum-column.js
@@ -2,26 +2,14 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-    await queryInterface.addColumn("WorkoutExercise", "orderNum", {
+    await queryInterface.addColumn('WorkoutExercise', 'orderNum', {
       type: Sequelize.INTEGER.UNSIGNED,
       allowNull: false,
-      defaultValue: 1
+      defaultValue: 1,
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-    await queryInterface.removeColumn("WorkoutExercise", "orderNum");
-  }
+    await queryInterface.removeColumn('WorkoutExercise', 'orderNum');
+  },
 };
